f170246de00948f39997d4cbaf68f391beb888a4
ineddieble51 <144681994+ineddieble51@users.noreply.github.com>
2023-10-26 13:14:40 +1100

    Update QuizPage.py modified

--- QuizPage.py
+++ QuizPage.py
@@ -1,93 +1,29 @@
 import tkinter as tk
-
+from module_dic import quiz
+from quizQuestionPage import quizQuestion
 
 class QuizPage(tk.Frame):
-    def __init__(self,master, user):
+    def __init__(self,master, user, module_frame):
         super().__init__(master)
         self.user = user
         self.master = master
+        self.module_frame = module_frame
 
-        quiz1_button = tk.Button(self.master, text = "Quiz 1", command= self.quiz_1)
-        quiz1_button.grid(row =0, column = 0 , padx =10, pady=10)
 
-        quiz2_button = tk.Button(self.master, text = "Quiz 2")
-        quiz2_button.grid(row =1, column = 0 , padx =10, pady=10)
+        for i in range (1,6):
+            quiz_button = tk.Button(self, text = f"Quiz {i}", command= lambda m= f"Quiz {i}":self.quiz(m))
+            quiz_button.grid(row =i, padx =10, pady=10)
 
-        quiz3_button = tk.Button(self.master, text = "Quiz 3")
-        quiz3_button.grid(row =2, column = 0 , padx =10, pady=10)
+        return_button =tk.Button(self, text="Return", command= self.module_frame_return)
+        return_button.grid(row = i+1, padx=10, pady=10)
 
-    def quiz_1(self):
+    def quiz(self, quiz_selected):
         self.place_forget()
-        quiz1_frame = tk.Toplevel(self.master)
-        quiz1_frame.title("Quiz 1")
+        quiz_frame = quizQuestion(self.master, self, quiz_selected, self.user)
+        quiz_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-        self.score = 0
-        self.current_question = 0
+    def module_frame_return(self):
+        self.place_forget()
+        self.module_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-        self.questions = [
-            {
-                "question": "What is Python?",
-                "options": ["Programming Language", "An animal", "A function", "A module"],
-                "correct_option": 0
-            },
-            {
-                "question": "What can Python do?",
-                "options": ["Python can supply power to the computer", "Python can create solid parts for 3D printing", "Python can be used for rapid prototyping, or for production-ready software development.", "Python can be used to chat with friends."],
-                "correct_option": 2
-            },
-            {
-                "question": "What will show on the cosole with this line of code (print('Hello, World!'))",
-                "options": ["Hello World", "Hello, World!", "print('Hello, World!)", "Print('Hello, World!)"],
-                "correct_option": 1
-            }
-        ]
-
-        self.question_label = tk.Label(quiz1_frame, text="", font=("Arial", 12))
-        self.question_label.pack(pady=10)
-
-        self.radio_var = tk.IntVar()
-
-        self.radio_buttons = []
-        for i in range(4):
-            radio_button = tk.Radiobutton(quiz1_frame, text="", variable=self.radio_var, value=i)
-            radio_button.pack()
-            self.radio_buttons.append(radio_button)
-
-        self.submit_button = tk.Button(quiz1_frame, text="Submit", command=self.check_answer)
-        self.submit_button.pack(pady=10)
-
-        self.next_question()
-
-    def next_question(self):
-        if self.current_question < len(self.questions):
-            question_data = self.questions[self.current_question]
-            self.question_label.config(text=question_data["question"])
-
-            for i in range(4):
-                self.radio_buttons[i].config(text=question_data["options"][i])
-
-            self.radio_var.set(-1)
-        else:
-            self.save_score()
-
-    def check_answer(self):
-        selected_option = self.radio_var.get()
-        correct_option = self.questions[self.current_question]["correct_option"]
-
-        if selected_option == correct_option:
-            self.score += 1
-
-        self.current_question += 1
-        self.next_question()
-
-    def save_score(self):
-        with open("quiz_score.txt", "w") as file:
-            file.write(f"{self.user.get_username()}: {self.score}/{len(self.questions)}")
         
-        self.question_label.config(text=f"Quiz completed! Your score: {self.score}/{len(self.questions)}")
-        for radio_button in self.radio_buttons:
-            radio_button.config(state="disabled")
-        self.submit_button.config(state="disabled")
-
-
-
