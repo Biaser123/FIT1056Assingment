66851d9c9b47d1aab501a8891f29c04b90e3b9f1
ryanle1308 <145404723+ryanle1308@users.noreply.github.com>
2023-10-29 22:48:32 +1100

    Update adminFrame.py

--- adminFrame.py
+++ adminFrame.py
@@ -13,30 +13,42 @@ class AdminFrame(MainPageFrame):
 
     def create_widgets(self):
 
-        custom_font = font.Font(size=16)
+        custom_font = font.Font(size=15)
 
         first_row_frame = tk.Frame(self)
         first_row_frame.pack(side="top")
 
-        profile_button = tk.Button(first_row_frame, text="Profile", command= self.view_profile,width=15,height=6,font=custom_font)
-        profile_button.pack(side="left", padx=10, pady=5)
+        self.profile_image = tk.PhotoImage(file="images/pngegg.png")
+        self.profile_image = self.profile_image.subsample(11) 
+        button = tk.Button(first_row_frame, text="Profile",command=self.view_profile, image=self.profile_image, compound=tk.TOP, width=140, height = 130, font=custom_font)
+        button.pack(side="left", padx=10, pady=5)
 
-        content_filter_button = tk.Button(first_row_frame, text="Content Filter",width=15,height=6,font=custom_font)
+        self.filter_image = tk.PhotoImage(file="images/filter.png")
+        self.filter_image = self.filter_image.subsample(11) 
+        content_filter_button = tk.Button(first_row_frame, text="Content Filter",image=self.filter_image, compound=tk.TOP, width=140, height = 130, font=custom_font)
         content_filter_button.pack(side="left", padx=10, pady=5)
 
-        manage_module_button = tk.Button(first_row_frame, text="Manage Module", command=self.manage_module,width=15,height=6,font=custom_font)
+        self.module_image = tk.PhotoImage(file="images/module.png")
+        self.module_image = self.module_image.subsample(13) 
+        manage_module_button = tk.Button(first_row_frame, text="Manage Module", command=self.manage_module,image=self.module_image, compound=tk.TOP, width=140, height = 130,font=custom_font)
         manage_module_button.pack(side="left", padx=10, pady=5)
 
         second_row_frame = tk.Frame(self)
         second_row_frame.pack(side="top")
 
-        manage_quiz_button = tk.Button(second_row_frame, text="Manage Quiz",width=15,height=6,font=custom_font)
+        self.challenge_image = tk.PhotoImage(file="images/challenge.png")
+        self.challenge_image = self.challenge_image.subsample(11) 
+        manage_quiz_button = tk.Button(second_row_frame, text="Manage Quiz",image=self.challenge_image, compound=tk.TOP, width=140, height = 130, font=custom_font)
         manage_quiz_button.pack(side="left", padx=10, pady=5)
 
-        manage_forum_button = tk.Button(second_row_frame, text="Manage Forum", command=self.manage_forum,width=15,height=6,font=custom_font)
+        self.forum_image = tk.PhotoImage(file="images/forum.png")
+        self.forum_image = self.forum_image.subsample(17) 
+        manage_forum_button = tk.Button(second_row_frame, text="Manage Forum", command=self.manage_forum,image=self.forum_image, compound=tk.TOP, width=140, height = 130, font=custom_font)
         manage_forum_button.pack(side="left", padx=10, pady=5)
 
-        user_activation_button = tk.Button(second_row_frame, text="Activate/Deactivate a User", command=self.user_activation,width=15,height=6,font=custom_font)
+        self.usermanage_image = tk.PhotoImage(file="images/management.png")
+        self.usermanage_image = self.usermanage_image.subsample(10) 
+        user_activation_button = tk.Button(second_row_frame, text="User management", command=self.user_activation,image=self.usermanage_image, compound=tk.TOP, width=140, height = 130, font=font.Font(size=13))
         user_activation_button.pack(side="left", padx=10, pady=5)
 
     def manage_module(self):
