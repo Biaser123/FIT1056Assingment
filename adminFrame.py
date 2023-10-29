1512ea943ed97b05d53eb120bed356fdca2d1dfb
ryanle1308 <145404723+ryanle1308@users.noreply.github.com>
2023-10-29 17:18:54 +1100

    Update adminFrame.py

--- adminFrame.py
+++ adminFrame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font
 
 from mainPageFrame import MainPageFrame
 from moduleManagementPage import ModuleManagementPage
@@ -10,18 +11,29 @@ class AdminFrame(MainPageFrame):
         super().__init__(user, master)
 
     def create_widgets(self):
-        super().create_widgets()
-        content_filter_button = tk.Button(self, text="Content Filter")
-        content_filter_button.pack(fill=tk.X, padx=10, pady=5)
 
-        manage_module_button = tk.Button(self, text="Manage Module", command=self.manage_module)
-        manage_module_button.pack(fill=tk.X, padx=10, pady=5)
+        custom_font = font.Font(size=16)
 
-        manage_quiz_button = tk.Button(self, text="Manage Quiz")
-        manage_quiz_button.pack(fill=tk.X, padx=10, pady=5)
+        first_row_frame = tk.Frame(self)
+        first_row_frame.pack(side="top")
 
-        manage_forum_button = tk.Button(self, text="Manage Forum", command=self.manage_forum)
-        manage_forum_button.pack(fill=tk.X, padx=10, pady=5)
+        profile_button = tk.Button(first_row_frame, text="Profile", command= self.view_profile,width=15,height=6,font=custom_font)
+        profile_button.pack(side="left", padx=10, pady=5)
+
+        content_filter_button = tk.Button(first_row_frame, text="Content Filter",width=15,height=6,font=custom_font)
+        content_filter_button.pack(side="left", padx=10, pady=5)
+
+        manage_module_button = tk.Button(first_row_frame, text="Manage Module", command=self.manage_module,width=15,height=6,font=custom_font)
+        manage_module_button.pack(side="left", padx=10, pady=5)
+
+        second_row_frame = tk.Frame(self)
+        second_row_frame.pack(side="top")
+
+        manage_quiz_button = tk.Button(second_row_frame, text="Manage Quiz",width=15,height=6,font=custom_font)
+        manage_quiz_button.pack(side="left", padx=10, pady=5)
+
+        manage_forum_button = tk.Button(second_row_frame, text="Manage Forum", command=self.manage_forum,width=15,height=6,font=custom_font)
+        manage_forum_button.pack(side="left", padx=10, pady=5)
 
     def manage_module(self):
         self.place_forget()
