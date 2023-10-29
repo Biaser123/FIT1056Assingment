003f2fa8d6ea0af9409c603057c422ecec919077
ryanle1308 <145404723+ryanle1308@users.noreply.github.com>
2023-10-30 02:59:00 +1100

    Update loginframe.py

--- loginframe.py
+++ loginframe.py
@@ -9,7 +9,8 @@ from adminFrame import AdminFrame
 from authenticator import Authenticator
 from teacherFrame import TeacherFrame
 from user import User
-
+y_positions = 645
+y_positions2 = 645
 
 class LoginFrame(tk.Frame):
     """
@@ -26,8 +27,8 @@ class LoginFrame(tk.Frame):
 
         # Logo image for the login page
         login_canvas = tk.Canvas(master=self, width=128, height=128)
-        login_canvas.grid(row=0, columnspan=2, sticky=tk.S, padx=10, pady=10)
-
+        login_canvas.grid(row=0, column=1,columnspan=2, sticky="s", padx=10, pady=10)
+        
         # Image obtained from:
         # https://www.veryicon.com/icons/healthcate-medical/medical-icon-two-color-icon/ico-health-clinic.html
         image_path = "./images/python-5-128.png"
@@ -40,38 +41,38 @@ class LoginFrame(tk.Frame):
         login_title = tk.Label(master=self,
                                text="Welcome to CodeVenture",
                                font=("Arial Bold", 25))
-        login_title.grid(row=1, columnspan=2, padx=10, pady=10)
+        login_title.grid(row=1, column=1, columnspan=2,padx=10, pady=10)
 
         # Label to ask user for Username
         username_label = tk.Label(master=self, text="Username:")
-        username_label.grid(row=2, column=0, sticky=tk.E, padx=10, pady=10)
+        username_label.grid(row=2, column=1, sticky="e",padx=10, pady=10)
 
         # Variable and entry for username
         self.username = tk.StringVar()
         username_entry = tk.Entry(master=self, textvariable=self.username)
-        username_entry.grid(row=2, column=1, sticky=tk.W, padx=10, pady=10)
+        username_entry.grid(row=2, column=2, sticky="w", padx=10, pady=10)
 
         # Label to ask user for Password
         password_label = tk.Label(master=self, text="Password:")
-        password_label.grid(row=3, column=0, sticky=tk.E, padx=10, pady=10)
+        password_label.grid(row=3, column=1, sticky=tk.E, padx=10, pady=10)
 
         # Variable and entry to password
         self.password = tk.StringVar()
         password_entry = tk.Entry(master=self, textvariable=self.password,
                                   show="●")
-        password_entry.grid(row=3, column=1, sticky=tk.W, padx=10, pady=10)
+        password_entry.grid(row=3, column=2, sticky=tk.W, padx=10, pady=10)
 
         # Button to login
         login_button = tk.Button(master=self, text="Login",
                                  command=self.authenticate_login)
-        login_button.grid(row=4, columnspan=2, padx=10, pady=10)
+        login_button.grid(row=4, column=1, columnspan=2, padx=10, pady=10)
 
         # Variable and label to inform user of login outcome
         self.login_text = tk.StringVar()
         # TODO: est. 2 lines
         login_message = tk.Message(master=self, textvariable=self.login_text,
                                    width=100)
-        login_message.grid(row=7, columnspan=2, padx=10, pady=10)
+        login_message.grid(row=6,column=1, columnspan=2, padx=10, pady=10)
 
         interface = self.master
         self.registration_window = tk.Toplevel(interface)
@@ -80,13 +81,13 @@ class LoginFrame(tk.Frame):
 
         # Title of Registration page
         registration_title = tk.Label(self.registration_window, text="Registration Page", font=("Arial Bold", 25))
-        registration_title.grid(row=0, column=1)
+        registration_title.grid(row=0, column=2)
         registration_title.grid_rowconfigure(1, weight=1)
         registration_title.grid_columnconfigure(1, weight=1)
 
         # Registration Button on Main Page
         register_button = tk.Button(master=self, text="Register", command=self.registration_page_open)
-        register_button.grid(row=5, columnspan=2, padx=10, pady=10)
+        register_button.grid(row=5,column=1, columnspan=2, padx=10, pady=10)
 
         # Registration Button on Registration Page
         register_button_reg = tk.Button(self.registration_window, text="Register", command=self.register_function)
@@ -145,6 +146,109 @@ class LoginFrame(tk.Frame):
         teacher_role = tk.Radiobutton(self.registration_window, variable=self.role_var, text="Teacher", value="Teacher")
         teacher_role.grid(row=7, column=2)
 
+        image_canvas = tk.Canvas(master=self, width =365, height=625)
+        image_canvas.grid(row=0,rowspan=10, column=0, padx=10, pady=10, sticky= "e")
+
+        def animation(self):
+            def two_loop():
+                square = image_canvas.create_rectangle(175, -150, 225, -100, fill="blue")
+                triangle = image_canvas.create_polygon(200,-80,175,-30,225,-30, fill="green")
+                circle = image_canvas.create_oval(175, -220, 225, -170, fill="red")
+                # Function to animate the shapes
+                def animate_shapes():
+                    y_positions2 = 0
+                    for i in range(3):
+                        image_canvas.move(shapes[i], 0, 1)
+                        y_positions2 += 1
+
+                        if y_positions2 >= 400:
+                            y_positions2 =0
+                            one_loop()
+
+                    image_canvas.after(10, animate_shapes)
+
+                # Start the animation
+                shapes = [square, triangle, circle]
+
+                animate_shapes()
+
+            def one_loop():
+                square = image_canvas.create_rectangle(175, -150, 225, -100, fill="blue")
+                triangle = image_canvas.create_polygon(200,-80,175,-30,225,-30, fill="green")
+                circle = image_canvas.create_oval(175, -220, 225, -170, fill="red")
+
+                # Function to animate the shapes
+                def animate_shapes():
+                    global y_positions
+                    for i in range(3):
+                        image_canvas.move(shapes[i], 0, 1)
+                        y_positions += 1
+
+                        if y_positions >= 645:
+                            y_positions = 0
+                            two_loop()
+
+                    image_canvas.after(10, animate_shapes)
+
+                # Start the animation
+                shapes = [square, triangle, circle]
+
+                animate_shapes()
+
+            one_loop()
+        animation(self)
+
+        image_canvas2 = tk.Canvas(master=self,height=625)
+        image_canvas2.grid(row=0,rowspan=10, column=3, padx=10, pady=10,sticky="e")
+
+        def animation2(self):
+            def two_loop():
+                square = image_canvas2.create_rectangle(150, -150, 200, -100, fill="blue")
+                triangle = image_canvas2.create_polygon(175,-80,150,-30,200,-30, fill="green")
+                circle = image_canvas2.create_oval(150, -220, 200, -170, fill="red")
+                # Function to animate the shapes
+                def animate_shapes():
+                    y_positions2 = 0
+                    for i in range(3):
+                        image_canvas2.move(shapes[i], 0, 1)
+                        y_positions2 += 1
+
+                        if y_positions2 >= 400:
+                            y_positions2 =0
+                            one_loop()
+
+                    image_canvas2.after(10, animate_shapes)
+
+                # Start the animation
+                shapes = [square, triangle, circle]
+
+                animate_shapes()
+
+            def one_loop():
+                square = image_canvas2.create_rectangle(150, -150, 200, -100, fill="blue")
+                triangle = image_canvas2.create_polygon(175,-80,150,-30,200,-30, fill="green")
+                circle = image_canvas2.create_oval(150, -220, 200, -170, fill="red")
+
+                # Function to animate the shapes
+                def animate_shapes():
+                    global y_positions2
+                    for i in range(3):
+                        image_canvas2.move(shapes[i], 0, 1)
+                        y_positions2 += 1
+
+                        if y_positions2 >= 645:
+                            y_positions2 = 0
+                            two_loop()
+
+                    image_canvas2.after(10, animate_shapes)
+
+                # Start the animation
+                shapes = [square, triangle, circle]
+
+                animate_shapes()
+
+            one_loop()
+        animation2(self)
     def registration_page_open(self):
         self.master.withdraw()
         self.registration_window.deiconify()
@@ -215,3 +319,8 @@ class LoginFrame(tk.Frame):
                 admin_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
         else:
             self.login_text.set("Failed to login")
+
+    
+
+
+
