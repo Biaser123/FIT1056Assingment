3de49c68f1c4fe9bbb741881278bc7729adf215b
ryanle1308 <145404723+ryanle1308@users.noreply.github.com>
2023-10-29 22:47:28 +1100

    Update StudentFrame.py

--- StudentFrame.py
+++ StudentFrame.py
@@ -19,32 +19,44 @@ class StudentFrame(MainPageFrame):
 
     def create_widgets(self):
 
-        custom_font = font.Font(size=16)
+        custom_font = font.Font(size=14)
 
         first_row_frame = tk.Frame(self)
         first_row_frame.pack(side="top")
 
-        profile_button = tk.Button(first_row_frame, text="Profile", command= self.view_profile,width=15,height=6,font=custom_font)
-        profile_button.pack(side="left", padx=10, pady=5)
+        self.profile_image = tk.PhotoImage(file="images/pngegg.png")
+        self.profile_image = self.profile_image.subsample(11) 
+        button = tk.Button(first_row_frame, text="Profile",command=self.view_profile, image=self.profile_image, compound=tk.TOP, width=140, height = 130, font=custom_font)
+        button.pack(side="left", padx=10, pady=5)
 
-        module_button = tk.Button(first_row_frame, text="Learning Module", command=self.learning_module,width=15,height=6,font=custom_font)
+        self.module_image = tk.PhotoImage(file="images/module.png")
+        self.module_image = self.module_image.subsample(13) 
+        module_button = tk.Button(first_row_frame, text="Learning Module", command=self.learning_module,image=self.module_image, compound=tk.TOP, width=140, height = 130,font=custom_font)
         module_button.pack(side="left", padx=10, pady=5)
 
-        take_quiz_button = tk.Button(first_row_frame, text="Take Challenge", command=self.take_challenge,width=15,height=6,font=custom_font)
+        self.challenge_image = tk.PhotoImage(file="images/challenge.png")
+        self.challenge_image = self.challenge_image.subsample(11) 
+        take_quiz_button = tk.Button(first_row_frame, text="Take Challenge", command=self.take_challenge,image=self.challenge_image, compound=tk.TOP, width=140, height = 130, font=custom_font)
         take_quiz_button.pack(side="left", padx=10, pady=5)
 
         second_row_frame = tk.Frame(self)
         second_row_frame.pack(side="top")
 
-        forum_button = tk.Button(second_row_frame, text="View Forum", command=self.view_forum,width=15,height=6,font=custom_font)
+        self.forum_image = tk.PhotoImage(file="images/forum.png")
+        self.forum_image = self.forum_image.subsample(17) 
+        forum_button = tk.Button(second_row_frame, text="View Forum", command=self.view_forum,image=self.forum_image, compound=tk.TOP, width=140, height = 130,font=custom_font)
         forum_button.pack(side="left", padx=10, pady=5)
 
-        feedback_button = tk.Button(second_row_frame, text="Feedback",command= self.feedback_page,width=15,height=6,font=custom_font)
+        self.feedback_image = tk.PhotoImage(file="images/feedback.png")
+        self.feedback_image = self.feedback_image.subsample(11) 
+        feedback_button = tk.Button(second_row_frame, text="Feedback",command= self.feedback_page,image=self.feedback_image, compound=tk.TOP, width=140, height = 130,font=custom_font)
         feedback_button.pack(side="left", padx=10, pady=20)
 
-        update_progress = tk.Button(second_row_frame, text="Update Progress", command =self.update_progress,width=15,height=6,font=custom_font)
+        self.progress_image = tk.PhotoImage(file="images/progress.png")
+        self.progress_image = self.progress_image.subsample(17) 
+        update_progress = tk.Button(second_row_frame, text="Update Progress", command =self.update_progress,image=self.progress_image, compound=tk.TOP, width=140, height = 130,font=custom_font)
         update_progress.pack(side="left", padx=10, pady=5)
-
+        
     def view_forum(self):
         forum_window = tk.Toplevel(self.master)
         forum_window.title("Forum")
